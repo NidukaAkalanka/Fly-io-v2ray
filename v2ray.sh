b402d3e2af1670006aaf00fe81b02b41346d055a
Niduka Akalanka <97357554+NidukaAkalanka@users.noreply.github.com>
2023-08-19 16:26:27 +0530

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -20,7 +20,6 @@ DGST_FILE="v2ray-linux-${ARCH}.zip.dgst"
 echo "Downloading binary file: ${V2RAY_FILE}"
 echo "Downloading binary file: ${DGST_FILE}"
 
-# TAG=$(wget -qO- https://raw.githubusercontent.com/v2fly/docker/master/ReleaseTag | head -n1)
 wget -O ${DOWNLOAD_PATH}/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${V2RAY_FILE} >/dev/null 2>&1
 wget -O ${DOWNLOAD_PATH}/v2ray.zip.dgst https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${DGST_FILE} >/dev/null 2>&1
 
@@ -34,17 +33,20 @@ LOCAL=$(openssl dgst -sha512 v2ray.zip | sed 's/([^)]*)//g')
 STR=$(cat < v2ray.zip.dgst | grep 'SHA512' | head -n1)
 
 if [ "${LOCAL}" = "${STR}" ]; then
-    echo " Check passed" && rm -fv v2ray.zip.dgst
+    echo " Check passed!" && rm -fv v2ray.zip.dgst
 else
-    echo " Check have not passed yet " && exit 1
+    echo " Check faild! Files unverified. " && exit 1
 fi
 
 # Prepare
-echo "Prepare to use"
+echo "Preparing to use"
 unzip v2ray.zip && chmod +x v2ray v2ctl
 mv v2ray v2ctl /usr/bin/
 mv geosite.dat geoip.dat /usr/local/share/v2ray/
-# mv config.json /etc/v2ray/config.json
+
+# Issue SSL Certificates
+echo "Issuing SSL Certificates"
+certbot certonly --standalone -d v2rayfly.adoadoxray.cf -m admin@adoadoxray.cf --agree-tos --no-eff-email
 
 # Set config file
 cat <<EOF >/etc/v2ray/config.json
@@ -55,19 +57,28 @@ cat <<EOF >/etc/v2ray/config.json
     "inbounds": [
         {
             "listen": "0.0.0.0",
-            "port": 8080,
+            "port": 443,
             "protocol": "vmess",
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "fa18d449-b591-46fe-8100-b2a672065774",
                         "alterId": 0
                     }
                 ],
-                "disableInsecureEncryption": true
+                "disableInsecureEncryption": false
             },
             "streamSettings": {
-                "network": "ws"
+                "network": "tls",
+                "security": "tls",
+                "tlsSettings": {
+                    "certificates": [
+                        {
+                            "certificateFile": "/etc/letsencrypt/live/v2rayfly.adoadoxray.cf/fullchain.pem",
+                            "keyFile": "/etc/letsencrypt/live/v2rayfly.adoadoxray.cf/privkey.pem"
+                        }
+                    ]
+                }
             }
         }
     ],
@@ -87,8 +98,10 @@ echo "Install done"
 echo "--------------------------------"
 echo "Fly App Name: ${FLY_APP_NAME}"
 echo "Fly App Region: ${FLY_REGION}"
-echo "V2Ray UUID: ${UUID}"
 echo "--------------------------------"
 
 # Run v2ray
 /usr/bin/v2ray -config /etc/v2ray/config.json
+
+# Add a cron job for Certbot renewal
+echo "0 0 * * * certbot renew --quiet" | crontab -
