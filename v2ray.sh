63c7096939c6aafa293a72338fb7e214182c3f0c
Niduka Akalanka <97357554+NidukaAkalanka@users.noreply.github.com>
2023-08-22 16:48:57 +0530

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
-##
 
 # Set ARG
-ARCH="64"
-DOWNLOAD_PATH="/tmp/v2ray"
-
-mkdir -p ${DOWNLOAD_PATH}
-cd ${DOWNLOAD_PATH} || exit
-
-TAG=$(wget --no-check-certificate -qO- https://api.github.com/repos/v2fly/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)
-if [ -z "${TAG}" ]; then
-    echo "Error: Get v2ray latest version failed" && exit 1
+PLATFORM=$1
+TAG=$2
+if [ -z "$PLATFORM" ]; then
+    ARCH="64"
+else
+    case "$PLATFORM" in
+        linux/386)
+            ARCH="32"
+            ;;
+        linux/amd64)
+            ARCH="64"
+            ;;
+        linux/arm/v6)
+            ARCH="arm32-v6"
+            ;;
+        linux/arm/v7)
+            ARCH="arm32-v7a"
+            ;;
+        linux/arm64|linux/arm64/v8)
+            ARCH="arm64-v8a"
+            ;;
+        *)
+            ARCH=""
+            ;;
+    esac
 fi
-echo "The v2ray latest version: ${TAG}"
+[ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
 
 # Download files
 V2RAY_FILE="v2ray-linux-${ARCH}.zip"
@@ -20,8 +35,8 @@ DGST_FILE="v2ray-linux-${ARCH}.zip.dgst"
 echo "Downloading binary file: ${V2RAY_FILE}"
 echo "Downloading binary file: ${DGST_FILE}"
 
-wget -O ${DOWNLOAD_PATH}/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${V2RAY_FILE} >/dev/null 2>&1
-wget -O ${DOWNLOAD_PATH}/v2ray.zip.dgst https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${DGST_FILE} >/dev/null 2>&1
+wget -O ${PWD}/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${V2RAY_FILE} > /dev/null 2>&1
+wget -O ${PWD}/v2ray.zip.dgst https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${DGST_FILE} > /dev/null 2>&1
 
 if [ $? -ne 0 ]; then
     echo "Error: Failed to download binary file: ${V2RAY_FILE} ${DGST_FILE}" && exit 1
@@ -29,26 +44,24 @@ fi
 echo "Download binary file: ${V2RAY_FILE} ${DGST_FILE} completed"
 
 # Check SHA512
-LOCAL=$(openssl dgst -sha512 v2ray.zip | sed 's/([^)]*)//g')
-STR=$(cat < v2ray.zip.dgst | grep 'SHA512' | head -n1)
+V2RAY_ZIP_HASH=$(sha512sum v2ray.zip | cut -f1 -d' ')
+V2RAY_ZIP_DGST_HASH=$(cat v2ray.zip.dgst | grep -e 'SHA512' -e 'SHA2-512' | head -n1 | cut -f2 -d' ')
 
-if [ "${LOCAL}" = "${STR}" ]; then
-    echo " Check passed!" && rm -fv v2ray.zip.dgst
+if [ "${V2RAY_ZIP_HASH}" = "${V2RAY_ZIP_DGST_HASH}" ]; then
+    echo " Check passed" && rm -fv v2ray.zip.dgst
 else
-    echo " Check faild! Files unverified. " && exit 1
+    echo "V2RAY_ZIP_HASH: ${V2RAY_ZIP_HASH}"
+    echo "V2RAY_ZIP_DGST_HASH: ${V2RAY_ZIP_DGST_HASH}"
+    echo " Check have not passed yet " && exit 1
 fi
 
 # Prepare
-echo "Preparing to use"
-unzip v2ray.zip && chmod +x v2ray v2ctl
-mv v2ray v2ctl /usr/bin/
+echo "Prepare to use"
+unzip v2ray.zip && chmod +x v2ray
+mv v2ray /usr/bin/
 mv geosite.dat geoip.dat /usr/local/share/v2ray/
-
-# Issue SSL Certificates
-# echo "Issuing SSL Certificates"
-# certbot certonly --standalone -d v2rayfly.adoadoxray.cf -m admin@adoadoxray.cf --agree-tos --no-eff-email
-
-# Set config file
+mv config.json /etc/v2ray/config.json
+> /etc/v2ray/config.json
 cat <<EOF >/etc/v2ray/config.json
 {
     "log": {
@@ -91,17 +104,6 @@ cat <<EOF >/etc/v2ray/config.json
 EOF
 
 # Clean
-cd ~ || return
-rm -rf ${DOWNLOAD_PATH:?}/*
-echo "Install done"
-
-echo "--------------------------------"
-echo "Fly App Name: ${FLY_APP_NAME}"
-echo "Fly App Region: ${FLY_REGION}"
-echo "--------------------------------"
-
-# Run v2ray
+rm -rf ${PWD}/*
 /usr/bin/v2ray -config /etc/v2ray/config.json
-
-# Add a cron job for Certbot renewal
-# echo "0 0 * * * certbot renew --quiet" | crontab -
+echo "Done"
